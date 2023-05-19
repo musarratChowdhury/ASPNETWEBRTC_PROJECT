d945ce9d3b69e529d83c527c588d21e5c0bd0e47
musarratChowdhury <muhitlopez143@gmail.com>
2023-05-19 15:45:14 +0600

    update

--- wwwroot/Scripts/game.ts
+++ wwwroot/Scripts/game.ts
@@ -1,12 +1,30 @@
-﻿
+﻿import $ from "jquery";
 
-import $ from 'jquery';
+var One = $('[data-number="1"]');
+var Two = $('[data-number="2"]');
+var Three = $('[data-number="3"]');
+var Four = $('[data-number="4"]');
+var Five = $('[data-number="5"]');
+var Six = $('[data-number="6"]');
 
-$(()=>{
-    console.log("hello");
-    console.log('how are you?');
-    
-    $(".one").on("click",()=>{
-        console.log("one clicked")
-    })
-})
+$(() => {
+  console.log("JS INITIATED");
+  One.on("click", () => {
+    console.log("one clicked");
+  });
+  Two.on("click", () => {
+    console.log("Two clicked");
+  });
+  Three.on("click", () => {
+    console.log("Three clicked");
+  });
+  Four.on("click", () => {
+    console.log("Four clicked");
+  });
+  Five.on("click", () => {
+    console.log("Five clicked");
+  });
+  Six.on("click", () => {
+    console.log("Six clicked");
+  });
+});
--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -1,8 +1,27 @@
-//
+var One = $('[data-number="1"]');
+var Two = $('[data-number="2"]');
+var Three = $('[data-number="3"]');
+var Four = $('[data-number="4"]');
+var Five = $('[data-number="5"]');
+var Six = $('[data-number="6"]');
 $(function () {
-    console.log("hello");
-    console.log('how are you?');
-    $(".one").on("click", function () {
-        console.log("one clicked");
-    });
+  console.log("JS INITIATED");
+  One.on("click", function () {
+    console.log("one clicked");
+  });
+  Two.on("click", function () {
+    console.log("Two clicked");
+  });
+  Three.on("click", function () {
+    console.log("Three clicked");
+  });
+  Four.on("click", function () {
+    console.log("Four clicked");
+  });
+  Five.on("click", function () {
+    console.log("Five clicked");
+  });
+  Six.on("click", function () {
+    console.log("Six clicked");
+  });
 });
